8507e863acadd04d6bb67be4af6c23eae9773b74
cinxini <kleaws@wni.com>
2024-08-14 11:45:12 +0900

    adding appbar

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -1,9 +1,18 @@
-import { createVuetify } from 'vuetify'
-import 'vuetify/styles'
+import '@fortawesome/fontawesome-free/css/all.css';
+import { createVuetify } from 'vuetify';
+import { aliases, fa } from 'vuetify/iconsets/fa';
+import 'vuetify/styles';
 
 export default defineNuxtPlugin((nuxtApp) => {
     const vuetify = createVuetify({
       // ... your configuration
+      icons: {
+        defaultSet: 'fa',
+        aliases,
+        sets: {
+          fa,
+        }
+      }
     })
     nuxtApp.vueApp.use(vuetify)
 })
